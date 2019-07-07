08e6b17730599008b661c858183b5b4e836abbd6
Eric Mantooth <erictooth@users.noreply.github.com>
2019-07-07 10:49:51 -0700

    Fix snapshot tests getting random ID every time

--- src/__tests__/Form.test.js
+++ src/__tests__/Form.test.js
@@ -6,32 +6,32 @@ import Form from "../index.js";
 
 const getTestComponent = () => (
     <Form layout="aligned" onSubmit={(e) => e.preventDefault()}>
-        <Form.Group>
+        <Form.Group id="1">
             <Form.Label>First Name</Form.Label>
             <Form.Control />
         </Form.Group>
-        <Form.Group>
+        <Form.Group id="2">
             <Form.Label>Last Name</Form.Label>
             <Form.Control />
         </Form.Group>
-        <Form.Group>
+        <Form.Group id="3">
             <Form.Label>Age</Form.Label>
             <Form.Control type="number" min="0" />
         </Form.Group>
-        <Form.Group>
+        <Form.Group id="4">
             <Form.Label>Bio</Form.Label>
             <Form.Control as="textarea" />
         </Form.Group>
-        <Form.Group>
+        <Form.Group id="5">
             <Form.Label>Email</Form.Label>
             <Form.Control type="email" />
             <small>You may occasionally receive marketing emails from us.</small>
         </Form.Group>
-        <Form.Group>
+        <Form.Group id="6">
             <Form.Control type="checkbox" />
             <Form.Label>Receive Newsletter</Form.Label>
         </Form.Group>
-        <Form.Group>
+        <Form.Group id="7">
             <Form.Label>Newsletter Frequency</Form.Label>
             <Form.Control as="select">
                 <option>Daily</option>
@@ -39,7 +39,7 @@ const getTestComponent = () => (
                 <option>Quarterly</option>
             </Form.Control>
         </Form.Group>
-        <Form.Group>
+        <Form.Group id="8">
             <span />
             <Form.Control as="button">Submit</Form.Control>
         </Form.Group>
