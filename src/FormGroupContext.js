dc52e6ba8771f28f4ec57366e4f51ebc6baa3951
Eric Mantooth <erictooth@users.noreply.github.com>
2019-07-07 10:57:23 -0700

    Remove unused GroupContext hook export

--- src/FormGroupContext.js
+++ src/FormGroupContext.js
@@ -1,5 +1,6 @@
 //@flow
 import * as React from "react";
+
 export type FormGroupContextValue = {|
     id: string,
     disabled: boolean,
@@ -7,13 +8,3 @@ export type FormGroupContextValue = {|
 |};
 
 export const FormGroupContext = React.createContext<?FormGroupContextValue>();
-
-export const useGroupContext = () => {
-    const groupContext = React.useContext(FormGroupContext);
-
-    if (!groupContext) {
-        throw new Error("useGroupContext must be used inside of a FormGroup");
-    }
-
-    return groupContext;
-};
